72b885ae0355f14bc1980ab7b4ff0764b0163915
Fitsum <fitbez@gmail.com>
2020-07-19 13:28:45 -0400

    Updates

---
+++ precache-manifest.05c75358547c54f51171d5ed8d1ba184.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a51e2a38615a1e8addab57c64f3c1312",
+    "url": "/my-todo-app/index.html"
+  },
+  {
+    "revision": "e0bb72f60628c154480b",
+    "url": "/my-todo-app/static/css/main.ea0cb16d.chunk.css"
+  },
+  {
+    "revision": "9fd254388e8bc84ce413",
+    "url": "/my-todo-app/static/js/2.d90f5eac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-todo-app/static/js/2.d90f5eac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0bb72f60628c154480b",
+    "url": "/my-todo-app/static/js/main.a3778120.chunk.js"
+  },
+  {
+    "revision": "a5752d071556986cf800",
+    "url": "/my-todo-app/static/js/runtime-main.43e49db3.js"
+  }
+]);
